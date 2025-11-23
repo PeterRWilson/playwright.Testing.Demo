ef18f20a32f735da6d5b5bbd624e42a655e8fc7d
Peter Wilson <peterrwilson97@gmail.com>
2025-11-23 21:50:15 +0000

    update error handling for delete pending pop up in teardown

--- helpers/userTeardown.ts
+++ helpers/userTeardown.ts
@@ -6,7 +6,8 @@ const teardown = async (page: Page): Promise<void> => {
   await page.goto("https://quotes.test.uinsure.co.uk/new-quote");
   await page.locator(mainPage.buildingContentsBtn).waitFor({ state: "visible" });
   await page.getByTestId("nav-drawer-item-pending-applications").click();
-  await page.waitForTimeout(1500);
+  await page.locator('[alt="Loading House"]').waitFor({ state: "hidden" });
+  await page.waitForTimeout(5000);
   if (await page.locator(pendingApplicationPage.quoteTable).isVisible()) {
     while (await page.locator(pendingApplicationPage.showMoreBtn).isVisible()) {
       await page.locator(pendingApplicationPage.showMoreBtn).click();
@@ -14,13 +15,20 @@ const teardown = async (page: Page): Promise<void> => {
     }
     let btnCount = await page.locator(pendingApplicationPage.viewApplicationBtn).count();
     while (btnCount > 0) {
-      const backdrop = page.locator(".MuiBackdrop-root");
-      if (await backdrop.isVisible()) {
-        await backdrop.waitFor({ state: "hidden", timeout: 10000 });
+      await page.locator(pendingApplicationPage.viewApplicationBtn).first().waitFor({ state: "visible" });
+      await page.locator(pendingApplicationPage.viewApplicationBtn).first().click({ force: true });
+      try {
+        await page.locator(pendingApplicationPage.deleteApplicationBtn).click({
+          force: true,
+          timeout: 5000
+        });
+      } catch (error) {
+        console.error("Error deleting pending application");
+        if (!page.isClosed()) {
+          await page.close();
+        }
+        return;
       }
-      await page.locator(pendingApplicationPage.viewApplicationBtn).waitFor({ state: "visible" });
-      await page.locator(pendingApplicationPage.viewApplicationBtn).click({ force: true });
-      await page.locator(pendingApplicationPage.deleteApplicationBtn).click({ force: true });
       await page.locator(pendingApplicationPage.yesDeleteBtn).click({ force: true });
       await page.waitForTimeout(1000);
       btnCount = await page.locator(pendingApplicationPage.viewApplicationBtn).count();
